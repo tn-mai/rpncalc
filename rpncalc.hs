8926bc36150d1f7ea8a892535ea5ae3889e5b2fa
tn_mai <rabitfiver@gmail.com>
2014-09-10 05:54:47 +0900

    First commit.

---
+++ rpncalc.hs
@@ -0,0 +1,24 @@
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+  s <- getArgs
+  let r = calc $ head s
+  case r of
+    Just value -> putStrLn $ show value
+    Nothing -> putStrLn "PARSE ERROR."
+
+calc :: String -> Maybe (Double)
+calc s = parse [] . reverse $ tokenize s
+
+parse :: [Double] -> [String] -> Maybe (Double)
+parse [] [] = Nothing
+parse stack [] = Just $ head stack
+parse (stack1:stack2:stackTail) ("*":xs) = parse ((stack1 * stack2) : stackTail) xs
+parse (stack1:stack2:stackTail) ("/":xs) = parse ((stack1 / stack2) : stackTail) xs
+parse (stack1:stack2:stackTail) ("+":xs) = parse ((stack1 + stack2) : stackTail) xs
+parse (stack1:stack2:stackTail) ("-":xs) = parse ((stack1 - stack2) : stackTail) xs
+parse stack (x:xs) = parse (read x:stack) xs
+
+tokenize :: String -> [String]
+tokenize s = foldr (\e (x:xs) -> if e == ' ' then []:x:xs else (x ++ [e]):xs) [[]] s
