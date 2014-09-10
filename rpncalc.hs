eb7716941ce32a763527291cd0a37cc4bf95cc45
tn_mai <rabitfiver@gmail.com>
2014-09-11 01:00:55 +0900

    Fix the RPN parser. I wrote on PN by mistake.

--- rpncalc.hs
+++ rpncalc.hs
@@ -9,15 +9,15 @@ main = do
     Nothing -> putStrLn "PARSE ERROR."
 
 calc :: String -> Maybe (Double)
-calc s = parse [] . reverse $ tokenize s
+calc s = parse [] $ tokenize s
 
 parse :: [Double] -> [String] -> Maybe (Double)
 parse [] [] = Nothing
 parse stack [] = Just $ head stack
-parse (stack1:stack2:stackTail) ("*":xs) = parse ((stack1 * stack2) : stackTail) xs
-parse (stack1:stack2:stackTail) ("/":xs) = parse ((stack1 / stack2) : stackTail) xs
-parse (stack1:stack2:stackTail) ("+":xs) = parse ((stack1 + stack2) : stackTail) xs
-parse (stack1:stack2:stackTail) ("-":xs) = parse ((stack1 - stack2) : stackTail) xs
+parse (stack1:stack2:stackTail) ("*":xs) = parse ((stack2 * stack1) : stackTail) xs
+parse (stack1:stack2:stackTail) ("/":xs) = parse ((stack2 / stack1) : stackTail) xs
+parse (stack1:stack2:stackTail) ("+":xs) = parse ((stack2 + stack1) : stackTail) xs
+parse (stack1:stack2:stackTail) ("-":xs) = parse ((stack2 - stack1) : stackTail) xs
 parse stack (x:xs) = parse (read x:stack) xs
 
 tokenize :: String -> [String]
