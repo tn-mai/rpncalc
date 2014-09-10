dc5d1e4b6ff5f0658b22fa57256357d1e3047db2
tn_mai <rabitfiver@gmail.com>
2014-09-11 01:57:35 +0900

    add the parse error message.

--- rpncalc.hs
+++ rpncalc.hs
@@ -1,17 +1,24 @@
 import System.Environment (getArgs)
+import Text.Read (readMaybe)
 
 main :: IO ()
 main = do
   s <- getArgs
-  putStrLn . show . head . foldl calcurate [] . words $ head s
+  let r = foldl calcurate (Just []) . words $ head s
+  case r of
+    Nothing -> putStrLn "PARSE ERROR."
+    Just n  -> putStrLn . show $ head n
 
-calcurate :: [Double] -> String -> [Double]
-calcurate xs "sum" = [sum xs]
-calcurate (x:xs) "ln" = log x:xs
-calcurate (x1:x2:xs) "^" = (x2 ** x1):xs
-calcurate (x1:x2:xs) "*" = (x2 * x1):xs
-calcurate (x1:x2:xs) "/" = (x2 / x1):xs
-calcurate (x1:x2:xs) "+" = (x2 + x1):xs
-calcurate (x1:x2:xs) "-" = (x2 - x1):xs
-calcurate xs e = (read e):xs
+calcurate :: Maybe ([Double]) -> String -> Maybe ([Double])
+calcurate Nothing _ = Nothing
+calcurate (Just xs) "sum" = Just [sum xs]
+calcurate (Just (x:xs)) "ln" = Just $ log x:xs
+calcurate (Just (x1:x2:xs)) "^" = Just $ (x2 ** x1):xs
+calcurate (Just (x1:x2:xs)) "*" = Just $ (x2 * x1):xs
+calcurate (Just (x1:x2:xs)) "/" = Just $ (x2 / x1):xs
+calcurate (Just (x1:x2:xs)) "+" = Just $ (x2 + x1):xs
+calcurate (Just (x1:x2:xs)) "-" = Just $ (x2 - x1):xs
+calcurate (Just xs) e = case readMaybe e of
+  Just n -> Just $ n:xs
+  Nothing -> Nothing
 
