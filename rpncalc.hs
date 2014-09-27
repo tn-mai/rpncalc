b6d1579f67639c536c9c17f3e4a833f0da1bedad
tn_mai <rabitfiver@gmail.com>
2014-09-28 03:16:43 +0900

    change to use foldM.

--- rpncalc.hs
+++ rpncalc.hs
@@ -1,24 +1,24 @@
 import System.Environment (getArgs)
 import Text.Read (readMaybe)
+import Control.Monad (foldM)
 
 main :: IO ()
 main = do
   s <- getArgs
-  let r = foldl calcurate (Right []) . words $ head s
+  let r = foldM calcurate [] . words $ head s
   case r of
     Left e  -> putStrLn $ "Parse error in '" ++ e ++ "'."
     Right n -> putStrLn . show $ head n
 
-calcurate :: (Either String [Double]) -> String -> (Either String [Double])
-calcurate (Left e) _ = Left e
-calcurate (Right xs) "sum" = Right [sum xs]
-calcurate (Right (x:xs)) "ln" = Right $ log x:xs
-calcurate (Right (x1:x2:xs)) "^" = Right $ (x2 ** x1):xs
-calcurate (Right (x1:x2:xs)) "*" = Right $ (x2 * x1):xs
-calcurate (Right (x1:x2:xs)) "/" = Right $ (x2 / x1):xs
-calcurate (Right (x1:x2:xs)) "+" = Right $ (x2 + x1):xs
-calcurate (Right (x1:x2:xs)) "-" = Right $ (x2 - x1):xs
-calcurate (Right xs) e = case readMaybe e of
+calcurate :: [Double] -> String -> (Either String [Double])
+calcurate xs "sum" = Right [sum xs]
+calcurate (x:xs) "ln" = Right $ log x:xs
+calcurate (x1:x2:xs) "^" = Right $ (x2 ** x1):xs
+calcurate (x1:x2:xs) "*" = Right $ (x2 * x1):xs
+calcurate (x1:x2:xs) "/" = Right $ (x2 / x1):xs
+calcurate (x1:x2:xs) "+" = Right $ (x2 + x1):xs
+calcurate (x1:x2:xs) "-" = Right $ (x2 - x1):xs
+calcurate xs e = case readMaybe e of
   Just n -> Right $ n:xs
   Nothing -> Left e
 
